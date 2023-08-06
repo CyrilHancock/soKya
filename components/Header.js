875e35463649ab79504473844216db799e816011
CyrilHancock <hancockcyril1910@gmail.com>
2023-08-06 16:34:01 +0530

    aaa

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,69 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
+import sokya from './Untitled.png'
+import { useEffect, useState } from 'react';
 export default function Header({ name }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [counter, setCounter] = useState(0);
+
+  const handleMouseEnter = () => {
+    setIsVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsVisible(false);
+    setCounter(0); // Reset the counter when the image disappears
+  };
+
+  const handleMouseMove = (event) => {
+    setPosition({ x: 0, y: event.clientY });
+  };
+
+  useEffect(() => {
+    const h1Element = document.getElementById('specific-h1');
+    h1Element.addEventListener('mouseenter', handleMouseEnter);
+    h1Element.addEventListener('mouseleave', handleMouseLeave);
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      h1Element.removeEventListener('mouseenter', handleMouseEnter);
+      h1Element.removeEventListener('mouseleave', handleMouseLeave);
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isVisible) {
+      const interval = setInterval(() => {
+        setCounter((prevCounter) => prevCounter + 1);
+        if(counter>innerWidth){
+          setPosition({ x: 0, y: 0});
+        }
+      }, 10);
+
+      return () => clearInterval(interval);
+    }
+  }, [isVisible]);
+
+  const imageStyle = {
+    transition: 'transform 0.3s ease',
+    transform: `translate(${counter}px, ${position.x}px)`,
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    zIndex: 9999,
+  };
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+       {isVisible && (
+       <div style={imageStyle}>
+      <Image src={sokya} alt="Profile Image"    width={20} height={20}  />
+    </div>
+       )}
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
-          <a>{name}</a>
+          <a id="specific-h1">{name}</a>
         </Link>
       </p>
     </header>
--- pages/index.js
+++ pages/index.js
@@ -11,42 +11,190 @@ import SEO from '../components/SEO';
 export default function Index({ posts, globalData }) {
   return (
     <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
+      <SEO title='So Kya' description='Plan to Victory' />
+      <Header name='So Kya'/>
       <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
+           Plan to Victory
         </h1>
         <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
-          ))}
+          <li className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <h2 className="text-2xl md:text-3xl">Preface:</h2>
+              <p className="mt-3 text-lg opacity-60">
+                <li>
+                  Each exercise should be performed based on one&apos;s strength
+                  and capabilities.
+                </li>
+                <li>
+                  If the advanced variation of an exercise is not possible, try
+                  easier variations.
+                </li>
+                <li>
+                  Prioritize mobility drills before exercise, and focus on
+                  maintaining proper form.
+                </li>
+                <li>
+                  Mobility drills will enhance flexibility and agility,
+                  preparing you for a successful workout.
+                </li>
+              </p>
+            </a>
+          </li>
+          <li className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <h2 className="text-2xl md:text-3xl">Mobility</h2>
+              <p className="mt-3 text-lg opacity-60">
+                Warming up is essential, especially for calisthenics training.
+                Spend a few minutes to get your body ready for the workout.
+                Choose a cardiovascular activity like running or jogging for 5-8
+                minutes to elevate your heart rate and warm up muscles, tendons,
+                and ligaments.
+              </p>
+            </a>
+          </li>
+          <li className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <h2 className="text-2xl md:text-3xl">Upper Body Mobility</h2>
+              <p className="mt-3 text-lg opacity-60">
+                <ul>
+                  <li>Scapula push-up</li>
+                  <li>Scapula pull-up</li>
+                  <li>Scapula foam rolling</li>
+                  <li>Armpit foam rolling</li>
+                  <li>Rotator cuff stretch</li>
+                  <li>Chest and shoulder stretch</li>
+                  <li>Shoulder dislocates</li>
+                </ul>
+              </p>
+            </a>
+          </li>
+          <li className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <h2 className="text-2xl md:text-3xl">Core Mobility</h2>
+              <p className="mt-3 text-lg opacity-60">
+                <ul>
+                  <li>Spine foam rolling</li>
+                  <li>Side leans</li>
+                </ul>
+              </p>
+            </a>
+          </li>
+          <li className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <h2 className="text-2xl md:text-3xl">Lower Body Mobility</h2>
+              <p className="mt-3 text-lg opacity-60">
+                <ul>
+                  <li>IT band foam rolling</li>
+                  <li>Adductor foam rolling</li>
+                  <li>Piriformis foam rolling</li>
+                  <li>Rollovers into straddle sit</li>
+                  <li>Knee circles</li>
+                  <li>Mountain climbers</li>
+                  <li>Frog hops</li>
+                  <li>Hip flexor stretch</li>
+                  <li>Deep squat position (beneficial for everyone)</li>
+                </ul>
+              </p>
+            </a>
+          </li>
+          <li className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <h2 className="text-2xl md:text-3xl">Flexibility</h2>
+              <p className="mt-3 text-lg opacity-60">
+                After your workout, spend a few minutes on these stretches,
+                holding each for 10-15 seconds
+              </p>
+            </a>
+          </li>
+          <li className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <h2 className="text-2xl md:text-3xl">Upper Body Stretches</h2>
+              <p className="mt-3 text-lg opacity-60">
+                <ul>
+                  <li>Chest and shoulder stretch</li>
+                  <li>Upper back stretch</li>
+                  <li>Chest stretch</li>
+                  <li>Forearm/wrist stretch 1</li>
+                  <li>Forearm/wrist stretch 2</li>
+                </ul>
+              </p>
+            </a>
+          </li>
+          <li className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <h2 className="text-2xl md:text-3xl">Core Stretches</h2>
+              <p className="mt-3 text-lg opacity-60">
+                <ul>
+                  <li>Standing side stretch</li>
+                  <li>Cobra stretch</li>
+                  <li>Cat stretch</li>
+                </ul>
+              </p>
+            </a>
+          </li>
+          <li className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <h2 className="text-2xl md:text-3xl">Lower Body Stretches</h2>
+              <p className="mt-3 text-lg opacity-60">
+                <ul>
+                  <li>Quad stretch</li>
+                  <li>Hamstring stretch</li>
+                  <li>Straddle stretch</li>
+                  <li>Groin stretch</li>
+                  <li>Hip flexor stretch</li>
+                  <li>Glute stretch</li>
+                  <li>Calf stretch</li>
+                </ul>
+              </p>
+            </a>
+          </li>
+          <li className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <h2 className="text-2xl md:text-3xl">
+                Exercise: (Monday, Wednesday, Friday)
+              </h2>
+              <p className="mt-3 text-lg opacity-60">
+                The Fundamental Five exercises are the cornerstone of
+                calisthenics training. Perform each exercise with proper form
+                and aim for the following repetitions:
+              <ul>
+                <li>Push-ups:</li>
+                <li className='text-sm'>Minimum of 20 perfect repetitions (2-3 sets)</li>
+                <li>
+                  Pull-ups:
+                  </li>
+                  <li className='text-sm'>
+                   Minimum of 10 perfect repetitions (2-3 sets); if no
+                  equipment, try bent over barbell row with a towel
+                </li>
+                <li>
+                  Triceps dips:
+                  </li>
+                  <li className='text-sm'>
+                   Minimum of 10 perfect repetitions (2-3 sets); if
+                  no equipment, try back dips
+                </li>
+                <li>
+                  Hanging knee raises:
+                  </li>
+                  <li className='text-sm'>
+                   Minimum of 10 perfect repetitions (2-3
+                  sets); if challenging, try reverse crunches
+                </li>
+                <li>Squats:
+                </li>
+                  <li className='text-sm'>
+                   Minimum of 25 perfect repetitions (2-3 sets)</li>
+                   
+              </ul>
+              </p>
+
+            </a>
+          </li>
         </ul>
       </main>
-      <Footer copyrightText={globalData.footerText} />
+      <Footer copyrightText='The only limit to our realization of tomorrow will be our doubts of today' />
       <GradientBackground
         variant="large"
         className="fixed top-20 opacity-40 dark:opacity-60"
